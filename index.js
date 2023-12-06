c091bf2d75a66fb3c59305c2330e380469d219b0
Jordy756 <83486124+Jordy756@users.noreply.github.com>
2023-12-06 17:23:53 -0600

    Todo

---
+++ index.js
@@ -0,0 +1,11 @@
+const graphics = document.querySelectorAll(".tableauPlaceholder");
+
+graphics.forEach(graphic => {
+    const divElement = document.getElementById(graphic.id);
+    const vizElement = divElement.getElementsByTagName("object")[0];
+    vizElement.style.width = "100%";
+    vizElement.style.height = divElement.offsetWidth * 0.75 + "px";
+    const scriptElement = document.createElement("script");
+    scriptElement.src = "https://public.tableau.com/javascripts/api/viz_v1.js";
+    vizElement.parentNode.insertBefore(scriptElement, vizElement);
+});
